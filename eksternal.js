e93b55850aa2bacd1951590a65fbfdb088604e66
HansMoses <114643572+HansMoses@users.noreply.github.com>
2022-10-05 13:08:35 +0700

    Update eksternal.js

--- eksternal.js
+++ eksternal.js
@@ -1,21 +1,15 @@
-<script src="http://code.jquery.com/jquery.min.js"></script>
-<canvas id="C" width="300px" height="300px" style="border:1px solid;"></canvas>
-<script>
-$(function(){
-var elem = document.getElementById("C");
-var rect = {x: 50, y: 50, w: 150, h: 130};
-
-var context = elem.getContext("2d");
-context.fillRect(rect.x, rect.y, rect.w, rect.h);
-
-$("#C").click(function(e) {
-        console.log("click: " + e.offsetX + "/" + e.offsetY);
-          if (rect.x+rect.w >= e.offsetX && rect.x <= e.offsetX && 
-           rect.y+rect.h >= e.offsetY && rect.y <= e.offsetY) {
-            console.log("collision");
-        } else {
-            console.log("no collision");
-        }
-    });
-});
-</script>
+function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('txt').innerHTML =
+        h + ":" + m + ":" + s;
+        var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
